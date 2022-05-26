ec9cad449f423744b004968e71baec37727a4af9
Júlio Mota <juliomotacampos@gmail.com>
2022-05-25 22:13:48 -0300

    feat: create footer component and added it to src/app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Mosaic from './components/Mosaic/Mosaic';
 import Videos from './components/Videos/Videos';
@@ -24,39 +25,7 @@ function App() {
       <Depositions />
       <SocialMedia />
       <Contact />
-
-      <footer>
-        <div className="container">
-          <div className="footer-block">
-            <h2>Telefone</h2>
-            <p>
-              (48)9999-9999
-              <br />
-              (48)9999-9999
-            </p>
-          </div>
-
-          <div className="footer-block">
-            <h2>Horários</h2>
-            <p>
-              Segunda a sexta
-              <br />
-              08:00 até 12:00
-            </p>
-          </div>
-
-          <div className="footer-block">
-            <h2>Endereço</h2>
-            <p>
-              Av. Trompowsky, 291. Torre 1 Sala 103
-              <br />
-              Cep 88015300 - Florianópolis, SC. 8802001
-            </p>
-          </div>
-
-          <div className="clear" />
-        </div>
-      </footer>
+      <Footer />
     </div>
   );
 }

b6cdab483f7da17cf912f056a719618d67d84e50
Júlio Mota <juliomotacampos@gmail.com>
2022-05-25 22:10:06 -0300

    feat: create social media component and added it to src/app

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Profile from './components/Profile/Profile';
 import Services from './components/Services/Services';
 import Treatments from './components/Treatments/Treatments';
 import Depositions from './components/Depositions/Depositions';
+import SocialMedia from './components/SocialMedia/SocialMedia';
 
 import './App.css';
 
@@ -20,40 +21,7 @@ function App() {
       <Videos />
       <Treatments />
       <Depositions />
-
-      <section className="social">
-        <div className="instagram-chamada">
-          <a target="_blank" rel="noreferrer" href="http://instagram.com"><img src="imagens/inst.png" alt="instagram" /></a>
-          <p>
-            Acesse o Instagram para ver mais fotos
-            <br />
-            @dra.fernanda_slovinski
-          </p>
-        </div>
-
-        <div className="mosaico-social">
-          <div className="img-mosaico-single-social">1</div>
-          <div className="img-mosaico-single-social">2</div>
-          <div className="img-mosaico-single-social">3</div>
-          <div className="img-mosaico-single-social">4</div>
-          <div className="img-mosaico-single-social">5</div>
-          <div className="img-mosaico-single-social">6</div>
-          <div className="img-mosaico-single-social">7</div>
-          <div className="img-mosaico-single-social">8</div>
-          <div className="img-mosaico-single-social">9</div>
-        </div>
-
-        <div className="facebook-chamada">
-          <a target="_blank" rel="noreferrer" href="http://facebook.com"><img src="imagens/fb.png" alt="facebook" /></a>
-          <p>
-            Acesse o Instagram para ver mais fotos
-            <br />
-            @dra.fernanda_slovinski
-          </p>
-        </div>
-
-        <div className="clear" />
-      </section>
+      <SocialMedia />
 
       <section className="contato">
         <h2>Contato</h2>
